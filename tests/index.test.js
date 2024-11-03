0fd54b6750996f93a9e98781cef0ead4b28ad816
Souradip <dgsouro77@gmail.com>
2024-11-03 23:58:06 +0530

    Added tests for sign up under authentication describe block

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,36 @@
+const { default: axios } = require("axios");
+
 function sum(a,b) {
     return a + b;
 }
 
-test('adds 1 + 2 to equal 3', () => {
-    let ans=sum(1,2);
-    expect(ans).toBe(3);
+const BACKEND_URL="http://localhost:3000"
 
-});
+//describe blocks
+describe("Authentication", () => {
+    test('User is able to sign up only once', async() => {;
+    const username="Souradip"+Math.random();    
+    const password="123456";
+
+    const response= await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+        username,
+        password,
+        type:"admin"
+    })
+
+    expect(response.statusCode).toBe(200); // new user signs up successfully
+ 
+    const updatedresponse= await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+        username,
+        password,
+        type:"admin"
+    })
+    expect(updatedresponse.statusCode).toBe(400); // user already signed up
+
+    });
+})
+
+
+
+
+
